b4dc5b089504944f6b6bc19a5650d0776183364a
Alex Tang <alexkai421@gmail.com>
2020-04-13 00:27:30 +0200

    FindActivity basic model

--- LFG/app/src/main/java/com/dat257/team1/LFG/MainActivity.java
+++ LFG/app/src/main/java/com/dat257/team1/LFG/MainActivity.java
@@ -2,15 +2,41 @@ package com.dat257.team1.LFG;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import com.dat257.team1.LFG.R;
+import com.dat257.team1.LFG.viewmodel.FindActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.FindActivities);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                openFindActivity();
+
+            }
+        });
+
+
+    }
+
+    public void openFindActivity(){
+
+        Intent intent = new Intent(this, FindActivity.class
+        );
+        startActivity(intent);
     }
 }
---
+++ LFG/app/src/main/java/com/dat257/team1/LFG/viewmodel/FindActivity.java
@@ -0,0 +1,52 @@
+package com.dat257.team1.LFG.viewmodel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+import com.dat257.team1.LFG.R;
+import com.dat257.team1.LFG.viewmodel.FindActivity;
+
+public class FindActivity extends AppCompatActivity {
+
+
+    private Button createActivity;
+    private Button menu;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find);
+        createActivity = (Button) findViewById(R.id.createActivity);
+        menu = (Button) findViewById(R.id.menu);
+        createActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHelloWorld();
+            }
+        });
+
+        menu.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openHelloWorld();
+
+            }
+        }));
+
+
+    }
+
+    public void openHelloWorld(){
+
+        Intent intent = new Intent(this, HelloWorld.class
+        );
+        startActivity(intent);
+    }
+}
