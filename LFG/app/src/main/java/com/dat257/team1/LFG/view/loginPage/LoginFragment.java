50f24d82c5341391c26482cd00b501262cc6e437
MiztaOak <ek.johan98@gmail.com>
2020-04-21 14:02:20 +0200

    Fixed some of the issues that that were introduced with the merge It is not perfect since the layout will need a lot more love to make it responsive to changes in the size of the screen. Other than that the rows that change to the feed if your already logged in are currently disabled because we have no logout function atm.
    Signed-off-by: MiztaOak <ek.johan98@gmail.com>

--- LFG/app/src/main/java/com/dat257/team1/LFG/view/loginPage/LoginFragment.java
+++ LFG/app/src/main/java/com/dat257/team1/LFG/view/loginPage/LoginFragment.java
@@ -1,23 +1,37 @@
 package com.dat257.team1.LFG.view.loginPage;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.dat257.team1.LFG.MainActivity;
 import com.dat257.team1.LFG.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 
 /**
  * A simple login on an account fragment
  *
- * @author : Jakobew
+ * @author : Jakobew, Oussama Anadani, Johan Ek
  */
 public class LoginFragment extends Fragment {
 
@@ -27,22 +41,104 @@ public class LoginFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.login_fragment, container, false);
+        final View rootView = inflater.inflate(R.layout.login_fragment, container, false);
 
         loginButton = rootView.findViewById(R.id.sign_in_button);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                emailField = view.findViewById(R.id.sign_in_email);
+                emailField = rootView.findViewById(R.id.sign_in_email);
                 String email = emailField.getText().toString().trim();
-                passwordField = view.findViewById(R.id.sign_in_pwd);
+                passwordField = rootView.findViewById(R.id.sign_in_pwd);
                 String password = passwordField.getText().toString().trim();
                 //checking if the user has entered a validate form of the email and password
-                //if (validateForm(email, password))
-                   // loginUser(email, password);
+                if (validateForm(email, password))
+                   loginUser(email, password);
             }
         });
 
+
         return rootView;
-}
+    }
+
+    /**
+     * A method to check the validation of the password and the email.
+     *
+     * @param email    The email that the user has entered.
+     * @param password The password that the user has entered.
+     * @return True if the form is validate and false otherwise.
+     */
+    private Boolean validateForm(String email, String password) {
+
+        if (TextUtils.isEmpty(email)) {
+            emailField.setError("Email is required!");
+            return false;
+        }
+        if (TextUtils.isEmpty(password)) {
+            passwordField.setError("Password is required!");
+            return false;
+        }
+        if (password.length() < 6) {
+            passwordField.setError("Password is too short!");
+            return false;
+        }
+        return true;
+    }
+
+
+    /**
+     * The login method that is connected to the LoginButton which authenticates
+     * with the database and gets the user's corresponding data depending on
+     * the user's password and email.
+     *
+     * @param email    The user's inserted email
+     * @param password The user's inserted password
+     */
+    public void loginUser(final String email, final String password) {
+        final FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {  // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(getActivity(), "Logged in successfully! ", Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            retrieveData(user);
+                            //take the user to the main page after successfully retrieving the data
+                            startActivity(new Intent(getActivity().getApplicationContext(), MainActivity.class));
+                        } else { // If sign in fails, display a message to the user.
+                            Toast.makeText(getActivity(), "Login failed! ", Toast.LENGTH_SHORT).show();
+                            //   retrieveData(null);
+                        }
+
+                    }
+                });
+    }
+
+    /**
+     * A method to retrieve the exciting user's data from the database
+     *
+     * @param currentUser The current user who's successfully logged in.
+     */
+    private void retrieveData(FirebaseUser currentUser) {
+        if (currentUser != null) {
+            FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+            DocumentReference documentReference = firestore.collection("users").document(currentUser.getUid());
+            documentReference.addSnapshotListener(getActivity(), new EventListener<DocumentSnapshot>() {
+                @Override
+                public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                    //todo retrieving the data
+                    /*
+                    fullName.setText(documentSnapshot.getString("name"));
+                    email.setText(documentSnapshot.getString("email"));
+                   // get the friendList, this should be in a recyclerView form
+                    */
+
+                }
+            });
+
+        }
+    }
+
+
 }
