a288bc5429dbcee4b1b159b7926cad4f80ffd720
Oussama Anadani <ousama.anadani@gmail.com>
2020-04-23 00:16:00 +0200

    Updated some minor things in the map

--- LFG/app/src/main/java/com/dat257/team1/LFG/service/activity_maps.java
+++ LFG/app/src/main/java/com/dat257/team1/LFG/service/activity_maps.java
@@ -1,10 +1,13 @@
 package com.dat257.team1.LFG.service;
 
+import android.location.Location;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
 import com.dat257.team1.LFG.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,13 +16,16 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
- * This class is responsible for displaying google maps fragment
+ * This class is responsible for displaying the address on google maps
  *
  * @author : Oussama Anadani
  */
 public class activity_maps extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Location currentLocation;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +38,11 @@ public class activity_maps extends FragmentActivity implements OnMapReadyCallbac
     }
 
 
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -45,11 +51,25 @@ public class activity_maps extends FragmentActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Gothenburg
+        LatLng location = new LatLng(57.708870, 11.974560);
+        markLocation(location);
+
     }
+
+    /**
+     * A method that marks the location on the map
+     *
+     * @param location The location that will be marked on the map
+     */
+    private void markLocation(LatLng location) {
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(location));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 14));
+        mMap.addMarker(new MarkerOptions().position(location).title("Activity here"));
+
+    }
+
 
 
 }
