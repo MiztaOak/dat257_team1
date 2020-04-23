ef81af5dfba6ca2532a9e56192db9b5069596cd2
Alex Tang <alexkai421@gmail.com>
2020-04-23 17:19:34 +0200

    A fragment for activity description created

--- LFG/app/src/main/java/com/dat257/team1/LFG/MainActivity.java
+++ LFG/app/src/main/java/com/dat257/team1/LFG/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 import com.dat257.team1.LFG.view.CreateActivityView;
 import com.dat257.team1.LFG.view.ActivityFeedView;
 import com.dat257.team1.LFG.view.loginPage.LoginPageView;
