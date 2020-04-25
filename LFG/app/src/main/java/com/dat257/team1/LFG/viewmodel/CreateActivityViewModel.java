4c79c8ce701340fca79d0ac43b5b4597056d59a3
gabjav <62600738+gabjav@users.noreply.github.com>
2020-04-25 20:08:22 +0200

    Update CreateActivityViewModel.java

--- LFG/app/src/main/java/com/dat257/team1/LFG/viewmodel/CreateActivityViewModel.java
+++ LFG/app/src/main/java/com/dat257/team1/LFG/viewmodel/CreateActivityViewModel.java
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint;
 import androidx.lifecycle.ViewModel;
 
 import com.dat257.team1.LFG.model.Main;
+import com.google.android.gms.games.multiplayer.Participant;
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.GeoPoint;
 import com.google.firebase.firestore.auth.User;
@@ -17,11 +18,16 @@ public class CreateActivityViewModel extends ViewModel {
     public CreateActivityViewModel() {
     }
 
-    public void createActivity(String title, String description, String address, String time) {
+    @SuppressLint("RestrictedApi") //Work-around caused by a bug that did not allow me to create a user
+    public void createActivity(String title, String description, String time, String address) {
         if (isValid()) {
             //Dummy ID, change later!
             String uID = "Dz0LrkQTOeefy7dqqx3E97xBHLE2";
-            Main.getInstance().createActivity(uID,title, description, address, time);
+            //Dummy participants, change later!
+            ArrayList<User> participants = new ArrayList<>();
+            //Dummy owner, change later!
+            User owner = new User("Dz0LrkQTOeefy7dqqx3E97xBHLE2");
+            Main.getInstance().createActivity(uID, owner, participants, title, description, time, address);
 
         }
     }
