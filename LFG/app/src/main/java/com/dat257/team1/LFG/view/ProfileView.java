3da62aaab5bb35ed1adae98737765c1fb8840638
jenzho <jjennie__@hotmail.com>
2020-05-08 19:01:12 +0200

    Added if-statement in profile view and a few changes in profile view

--- LFG/app/src/main/java/com/dat257/team1/LFG/view/ProfileView.java
+++ LFG/app/src/main/java/com/dat257/team1/LFG/view/ProfileView.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dat257.team1.LFG.R;
+import com.dat257.team1.LFG.firebase.FireStoreHelper;
+import com.dat257.team1.LFG.model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileView extends AppCompatActivity {
     private ImageView profileImage;
@@ -19,19 +24,35 @@ public class ProfileView extends AppCompatActivity {
 
     private TextView userName;
     private TextView profileDescription;
+    private TextView emailTextView;
+    private TextView phoneTextView;
 
     private Button addFriendButton;
     private Button blockContactButton;
 
+    private LinearLayout addFriendLayout;
+    private LinearLayout blockContactLayout;
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
         initViews();
 
-        //TODO: fetch user information from database
+        FireStoreHelper.getInstance(); //TODO: There's no method for retrieving user information from db
+        User currentUser = null; // You
+        User profileUser = null; // User who owns the profile
+
+
+        if (currentUser.getEmail().equals(profileUser.getEmail())) {
+            addFriendLayout.setVisibility(View.INVISIBLE);
+            blockContactLayout.setVisibility(View.INVISIBLE);
+        } else {
+            addFriendLayout.setVisibility(View.VISIBLE);
+            blockContactLayout.setVisibility(View.VISIBLE);
+        }
 
         addFriendButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,5 +77,9 @@ public class ProfileView extends AppCompatActivity {
         blockContactImage = findViewById(R.id.block_contact);
         profileDescription = findViewById(R.id.profile_desc);
         userName = findViewById(R.id.user_name_profile);
+        emailTextView = findViewById(R.id.email_text_view);
+        phoneTextView = findViewById(R.id.phone_text_view);
+        addFriendLayout = findViewById(R.id.linearLayout3);
+        blockContactLayout = findViewById(R.id.linearLayout4);
     }
 }
